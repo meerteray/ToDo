7cbd4aee8156ebf766f8b34a551785e73d1eec45
Mert Eray <meerteray@gmail.com>
2024-03-09 18:26:27 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Mert Eray on 9.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
